081ec4e606175b2cf13dd0490701b1c566591672
min_xu <xumin_wlt@163.com>
2016-09-26 15:36:45 +0800

    mongo storage success test

---
+++ j360-trace-storage/mongodb/src/main/java/me/j360/trace/storage/mongodb/DependencyLinkSpanInterator.java
@@ -0,0 +1,10 @@
+package me.j360.trace.storage.mongodb;
+
+/**
+ * Package: me.j360.trace.storage.mongodb
+ * User: min_xu
+ * Date: 2016/9/26 下午3:35
+ * 说明：
+ */
+public class DependencyLinkSpanInterator {
+}
---
+++ j360-trace-storage/mongodb/src/main/java/me/j360/trace/storage/mongodb/MongoStorage.java
@@ -0,0 +1,10 @@
+package me.j360.trace.storage.mongodb;
+
+/**
+ * Package: me.j360.trace.storage.mongodb
+ * User: min_xu
+ * Date: 2016/9/26 下午3:34
+ * 说明：
+ */
+public class MongoStorage {
+}
