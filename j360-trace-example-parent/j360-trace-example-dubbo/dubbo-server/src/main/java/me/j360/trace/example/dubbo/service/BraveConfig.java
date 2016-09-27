292df4aaefa986a9b4020826e4e6696ab19ced82
min_xu <xumin_wlt@163.com>
2016-09-27 15:08:21 +0800

    intergration kafkaSpanCollection with dubbo

--- j360-trace-example-parent/j360-trace-example-dubbo/dubbo-server/src/main/java/me/j360/trace/example/dubbo/service/BraveConfig.java
+++ j360-trace-example-parent/j360-trace-example-dubbo/dubbo-server/src/main/java/me/j360/trace/example/dubbo/service/BraveConfig.java
@@ -1,6 +1,8 @@
 package me.j360.trace.example.dubbo.service;
 
 import me.j360.trace.collector.core.Brave;
+import me.j360.trace.collector.core.EmptySpanCollectorMetricsHandler;
+import me.j360.trace.collector.kafka.KafkaSpanCollector;
 import me.j360.trace.spring.core.BraveApiConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,10 +18,18 @@ import org.springframework.context.annotation.Import;
 @Import(BraveApiConfig.class)
 public class BraveConfig {
 
-    @Bean
+    /*@Bean
     public Brave brave() {
         Brave.Builder builder = new Brave.Builder("J360DubboInterceptorIntegration")
                 .spanCollector(SpanCollectorForTesting.getInstance());
+        return builder.build();
+    }*/
+
+
+    @Bean
+    public Brave brave() {
+        Brave.Builder builder = new Brave.Builder("J360DubboInterceptorIntegration")
+                .spanCollector(KafkaSpanCollector.create("172.16.10.125:9092", new EmptySpanCollectorMetricsHandler()));
         return builder.build();
     }
 
