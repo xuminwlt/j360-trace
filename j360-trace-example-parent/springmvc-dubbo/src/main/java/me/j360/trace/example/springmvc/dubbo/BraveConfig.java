b6a7f05ae99005bffe447f7682c1da373470dcfd
min_xu <xumin_wlt@163.com>
2016-09-27 16:29:11 +0800

    intergration springmvc dubbo trace tree

---
+++ j360-trace-example-parent/springmvc-dubbo/src/main/java/me/j360/trace/example/springmvc/dubbo/BraveConfig.java
@@ -0,0 +1,49 @@
+package me.j360.trace.example.springmvc.dubbo;
+
+import me.j360.trace.collector.core.*;
+import me.j360.trace.collector.kafka.KafkaSpanCollector;
+import me.j360.trace.http.DefaultSpanNameProvider;
+import me.j360.trace.spring.core.BraveApiConfig;
+import me.j360.trace.springmvc.ServletHandlerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@Import(BraveApiConfig.class)
+@ImportResource("classpath:/client.xml")
+@EnableWebMvc
+public class BraveConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private ServerRequestInterceptor requestInterceptor;
+
+    @Autowired
+    private ServerResponseInterceptor responseInterceptor;
+
+    @Autowired
+    private ServerSpanThreadBinder serverThreadBinder;
+
+    @Bean
+    public Brave brave() {
+        Brave.Builder builder = new Brave.Builder("J360ServletInterceptorIntegration")
+                .spanCollector(KafkaSpanCollector.create("172.16.10.125:9092", new EmptySpanCollectorMetricsHandler()));
+        return builder.build();
+    }
+
+    @Bean
+    public PingController pingController() {
+        return new PingController();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ServletHandlerInterceptor(requestInterceptor, responseInterceptor, new DefaultSpanNameProvider(), serverThreadBinder));
+    }
+
+}
